fe55e9e185142ddd246a470a0d3079716fc3da78
scy1030503929 <1030503929@qq.com>
2019-04-18 14:12:11 +0800

    短网址完成

--- src/ShortUrl/ShortUrl.php
+++ src/ShortUrl/ShortUrl.php
@@ -7,38 +7,38 @@
  * Time: 下午4:57
  */
 
+namespace ShortUrl;
+
+/**
+ * Class ShortUrl
+ * @package ShortUrl
+ */
+
 class ShortUrl
 {
     /**
-     * 生成短网址
-     * @param string $longUrl 长网址
-     * @return array 短网址
+     * 短网址
+     * @param string $long_url 长链接
+     * @return mixed
      */
-    public static function toShortUrl($longUrl = ''){
-        $base32 = array(
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-            'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-            'y', 'z', '0', '1', '2', '3', '4', '5'
-        );
-
-        $hex = md5($longUrl);
-        $hexLen = strlen($hex);
-        $subHexLen = $hexLen / 8;
-        $shortUrl = array();
-        for ($i = 0; $i < $subHexLen; $i++) {
-            // 把加密字符按照8位一组16进制与0x3FFFFFFF(30位1)进行位与运算
-            $subHex = substr($hex, $i * 8, 8);
-            $int = 0x3FFFFFFF & (1 * ('0x' . $subHex));
-            $out = '';
-            for ($j = 0; $j < 6; $j++) {
-                // 把得到的值与0x0000001F进行位与运算，取得字符数组chars索引
-                $val = 0x0000001F & $int;
-                $out .= $base32[$val];
-                $int = $int >> 5;
-            }
-            $shortUrl[] = $out;
+    public static function shortUrl($long_url = '')
+    {
+        if (!$long_url) {
+            return self::toJson(404, 'failed', '');
         }
-        return $shortUrl;
+        $app_key = 'c5faa6d67402859b6366ada95671c017';
+        $long_url = urlencode($long_url);
+        $url = "http://www.mynb8.com/api2/sina?appkey=" . $app_key . "&long_url=" . $long_url;
+        $json = file_get_contents($url);
+        $url = json_decode(stripslashes($json), true);
+        return self::toJson(0,'success',$url['short_url']);
+    }
+
+    public function toJson($code = 0, $msg = '', $url = '')
+    {
+        $result['code'] = $code;
+        $result['msg'] = $msg;
+        $result['url'] = $url;
+        return $result;
     }
 }
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: mingbai
+ * Date: 19-4-11
+ * Time: 下午5:49
+ */
+require_once './vendor/autoload.php';
+use ShortUrl\ShortUrl;
+return ShortUrl::shortUrl("https://fx.diancang.site/blog/index/q2OgrdUkV9AN");
